dfae6c128a72d9abb8540bb9e548e1b74475b22f
crossfire3001 <obiwan2035@mail.ru>
2023-10-19 19:05:27 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+document.getElementById('choose-pizza').onclick = function () {
+    document.getElementsByClassName('products')[0].scrollIntoView({
+        block: 'center',
+        behavior: 'smooth'
+    });
+}
+
+let productInput = document.getElementById('product-input');
+let addToCartButtons = document.getElementsByClassName('btn-add-to-cart');
+
+for (let i = 0; i < addToCartButtons.length; i++) {
+    addToCartButtons[i].onclick = function (e) { productInput.value = e.target.parentElement.previousElementSibling.previousElementSibling.innerText;
+        document.getElementsByClassName('order')[0].scrollIntoView({
+            block: 'center',
+            behavior: 'smooth'
+        });
+    }
+}
+
+document.getElementById('create-order').onclick = function () {
+    let addressInput = document.getElementById('address-input');
+    let phoneInput = document.getElementById('phone-input');
+    let orderFilled = false;
+
+    if (!productInput.value) {
+        alert('Заполните пиццу');
+        return;
+    }
+    if (!addressInput.value) {
+        alert('Заполните заполните адрес');
+        return;
+    }
+    if (!phoneInput.value) {
+        alert('Заполните телефон');
+        return;
+    }
+
+    if (!orderFilled) {
+        [productInput, addressInput, phoneInput].forEach((item) => {
+            item.value = "";
+        });
+        alert("Спасибо за заказ!");
+    }
+}
+
+element.onclick = function () { alert(1); }
+element.click(function() { alert(1); })
