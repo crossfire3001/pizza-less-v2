e4f29f03847ebbd28ce384591f5dc6134f74fa3e
crossfire3001 <obiwan2035@mail.ru>
2024-01-08 16:44:27 +0500

    Modify address input validation and comment out phone validation
    This commit modifies the validation of the 'addressInput' field to include a regular expression validation. The code change also comments out the 'phoneInput' validation in the script.js file, while moving the phone number format requirement to the HTML layer, improving the reliability and user experience of form inputs.

--- js/script.js
+++ js/script.js
@@ -26,14 +26,14 @@ document.getElementById('create-order').onclick = function () {
         alert('Заполните пиццу');
         return;
     }
-    if (!addressInput.value) {
+    if (!addressInput.value.match(/^[а-яА-Я0-9,.\s]+$/)) {
         alert('Заполните заполните адрес');
         return;
     }
-    if (!phoneInput.value) {
+    /* if (!phoneInput.value.match(/^\+7\s\(9\d{2}\)\s\d{3}-\d{2}-\d{2}/)) {
         alert('Заполните телефон');
         return;
-    }
+    } */
 
     if (!orderFilled) {
         [productInput, addressInput, phoneInput].forEach((item) => {
